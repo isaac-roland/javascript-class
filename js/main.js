46c19db779d347d122aed29e945f4fa4d579a9bd
isaac-roland <iroland@therolandfamily.net>
2017-08-16 10:16:08 -0500

    fixed background

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 var GameState = {
     preload: function() {
-        this.load.image('background', 'pictures/bugatti_chiron.jpg');
+        this.load.image('background', 'pictures/epic_face.png');
         this.load.image('arrow', 'pictures/super_short_name.jpg')
         this.load.image('invader', 'pictures/invader.jpg');
         this.load.image('epicFace', 'pictures/epic_face.png');
         this.load.image('outerSpace', 'pictures/invader.jpg');
         this.load.image('theFaceOfEpicness', 'pictures/super_short_name.jpg');
+        this.load.image('rightArrow', 'Desktop/arrow.png');
         //this.load.image('theFaceOfEpicness', 'pictures/super_short_name.jpeg');
     },
     create: function() {
@@ -13,19 +14,21 @@ var GameState = {
         this.background = this.game.add.sprite(10, 10, 'background');
         this.invader = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'invader');
         this.invader.anchor.setTo(0.5, 0.5);
-        this.invader.scale.setTo(1, 1);
-        this.rightArrow = this.game.add.sprite(580, this.game.world.centerY, 'arrow');
-        this.epicFace = this.game.add.sprite(120, 10, 'epicFace');
-        this.epicFace.scale.setTo(0.1);
-        this.outerSpace = this.game.add.sprite(500, 300, 'outerSpace');
-        this.outerSpace.anchor.setTo(0.5);
-        this.outerSpace.scale.setTo(-1, 1);
-        this.theFaceOfEpicness = this.game.add.sprite(100, 25, 'theFaceOfEpicness');
-        this.theFaceOfEpicness.scale.setTo(.5);
-        this.theFaceOfEpicness.angle = 90;
+        this.rightArrow = this.game.add.sprite(580, this,game.world.centerY, 'arrow');
+        this.rightArrow
+        // this.invader.scale.setTo(1, 1);
+        // this.rightArrow = this.game.add.sprite(580, this.game.world.centerY, 'arrow');
+        // this.epicFace = this.game.add.sprite(120, 10, 'epicFace');
+        // this.epicFace.scale.setTo(0.1);
+        // this.outerSpace = this.game.add.sprite(500, 300, 'outerSpace');
+        // this.outerSpace.anchor.setTo(0.5);
+        // this.outerSpace.scale.setTo(-1, 1);
+        // this.theFaceOfEpicness = this.game.add.sprite(100, 25, 'theFaceOfEpicness');
+        // this.theFaceOfEpicness.scale.setTo(.5);
+        // this.theFaceOfEpicness.angle = 90;
     }, 
     update: function() {
-        this.theFaceOfEpicness.angle += 0.5;
+        // this.theFaceOfEpicness.angle += 0.5;
     }
 };
 
@@ -35,9 +38,7 @@ game.state.add('GameState', GameState);
 
 game.state.start('GameState');
 
-//var i = "WATTS up, thats quite SHOCKING, I'm glad I'm not in your CURRENT situation, don't you wanna go HOME? im quite conFUSED";
-
-//alert(i);
+var i = "WATTS up, thats quite SHOCKING, I'm glad I'm not in your CURRENT situation, don't you wanna go HOME? im quite conFUSED";
 
 var x = -1;
 
